00682474fef6cb30f30e825c1b4c0a31cb413c32
Sruthi Sudhakar <sruthis@gatech.edu>
2019-02-15 20:47:10 -0500

    created jsonserializer class

--- java-source/src/CLI/CLIRunner.java
+++ java-source/src/CLI/CLIRunner.java
@@ -1,18 +1,25 @@
 package CLI;
 
-import picocli.CommandLine;
-
 import java.util.concurrent.Callable;
 
+//
+//import picocli.CommandLine;
+//
+//import java.util.concurrent.Callable;
+//
 public class CLIRunner implements Callable<Void> {
-    public static void main(String[] args) throws Exception {
-        CommandLine.call(new CLIRunner(), args);
-    }
-
     @Override
     public Void call() throws Exception {
-        System.out.println("TEST");
         return null;
     }
-
+//    public static void main(String[] args) throws Exception {
+//        CommandLine.call(new CLIRunner(), args);
+//    }
+//
+//    @Override
+//    public Void call() throws Exception {
+//        System.out.println("TEST");
+//        return null;
+//    }
+//
 }
--- java-source/src/graph/JsonSerializer.java
+++ java-source/src/graph/JsonSerializer.java
@@ -1,7 +1,24 @@
 package graph;
 
+import java.io.*;
+
 public class JsonSerializer {
     public static void main(String[] args) {
-
+        String fileName = "C:\\Georgia Tech\\bigDataClub\\Bus Route Optimization\\bus-route-optimizer\\fakeStuff\\fakeGraphAdjacencyList.json";
+        BufferedReader br = null;
+        String line = "";
+        String jsonString = "";
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null) {
+                jsonString += line;
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(jsonString);
     }
 }
