7ce3d1c2e12e7dbb1f40646f3b7a521993dd9ae2
REOTyranny <reotyranny@gmail.com>
2019-02-18 19:34:04 -0500

    added Bus stop metadata class

---
+++ java-source/src/bus_route_metadata/BusStopMetadata.java
@@ -0,0 +1,12 @@
+package bus_route_metadata;
+
+public class BusStopMetadata {
+    String name;
+    public BusStopMetadata(String name){
+        this.name = name;
+
+    }
+    public String getName(){
+        return name;
+    }
+}
