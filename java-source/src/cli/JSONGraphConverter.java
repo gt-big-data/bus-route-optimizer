6d6993ae2d969760968df1dec1cba7027641fe8c
deb <debkbanerji@gmail.com>
2019-02-18 19:33:34 -0500

    Push skeleton code

--- java-source/src/cli/JSONGraphConverter.java
+++ java-source/src/cli/JSONGraphConverter.java
@@ -4,4 +4,10 @@ import graph.DirectedGraph;
 
 public class JSONGraphConverter {
 
+    public static DirectedGraph generateGraphFromJSON(String inputAdjacencyListFile, String inputColoringFile) {
+        return null;
+    }
+
+    public static void generateJSONsFromGraph(DirectedGraph inputGraph, String outputAdjacencyListFile, String outputColoringFile) {
+    }
 }
