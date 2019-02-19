f57b90e0bb8747c1cac1d9dda07390b0c9dd1e8e
deb <debkbanerji@gmail.com>
2019-02-18 19:24:36 -0500

    Add parameters

--- java-source/src/cli/CLIRunner.java
+++ java-source/src/cli/CLIRunner.java
@@ -6,6 +6,18 @@ import java.util.concurrent.Callable;
 
 public class CLIRunner implements Callable<Void> {
 
+    @CommandLine.Parameters(paramLabel = "input_adjacency_list_file", description = "Input adjacency list JSON file.", index = "0")
+    private String inputAdjacencyListFile;
+
+    @CommandLine.Parameters(paramLabel = "output_adjacency_list_file", description = "Output adjacency list JSON file.", index = "1")
+    private String outputAdjacencyListFile;
+
+    @CommandLine.Parameters(paramLabel = "input_coloring_file", description = "Input coloring JSON file.", index = "2")
+    private String inputColoringFile;
+
+    @CommandLine.Parameters(paramLabel = "output_coloring_file", description = "Output coloring JSON file.", index = "3")
+    private String outputColoringFile;
+
     public static void main(String[] args) throws Exception {
         CommandLine.call(new CLIRunner(), args);
     }
