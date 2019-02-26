e03b95ee020b242a46ccbd9bfe1d01278b77f7a7
Sruthi Sudhakar <sruthis@gatech.edu>
2019-02-25 19:08:03 -0500

    reading in json

--- java-source/src/cli/JSONGraphConverter.java
+++ java-source/src/cli/JSONGraphConverter.java
@@ -1,10 +1,25 @@
 package cli;
 
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
+import com.google.gson.stream.JsonReader;
 import graph.DirectedGraph;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class JSONGraphConverter {
 
     public static DirectedGraph generateGraphFromJSON(String inputAdjacencyListFile, String inputColoringFile) {
+        try {
+            Gson gson = new Gson();
+            JsonReader jsonReader = new JsonReader(new FileReader(inputAdjacencyListFile));
+            LinkedTreeMap data = gson.fromJson(jsonReader, LinkedTreeMap.class);
+            
+//            System.out.println(data);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
